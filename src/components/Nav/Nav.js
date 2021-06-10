8956548356783e8dd5072236d4afaecac04b919e
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-10 15:30:20 +0200

    Testing how a CSS file for a component works with webpack

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import './Nav.scss';
 
 const Nav = () => {
   return (
     <nav>
-      <h1>RESTAURANT</h1>
+      <h1 className="header">RESTAURANT</h1>
       <button>About</button>
       <button>Menus</button>
       <button>Contact</button>
