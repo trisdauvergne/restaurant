07841fb2508c1159f7678b0f5f42f7162a902c9c
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-10 16:33:21 +0200

    Experimenting with transitions on scroll

--- src/pages/menus/Menus.js
+++ src/pages/menus/Menus.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './menus.scss';
 
 const Menus = () => {
+  const [menu, setMenu] = useState(false);
+
+  const changeBackground = () => {
+    // console.log(window.scrollY);
+    if(window.scrollY >= 550) {
+      setMenu(true);
+    } else {
+      setMenu(false);
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground);
+
   return (
-    <section id="menus">
+    <section className={menu ? "menus active" : "menus"} id="menus">
       <h1>Menus page</h1>
     </section>
   )
