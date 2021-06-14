25aca3f476f5b7b573c50ad100ae45ae72b8f349
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-14 18:08:04 +0200

    Added a useAnimation experiment to contact.js

--- src/components/lunchMenu/LunchMenu.js
+++ src/components/lunchMenu/LunchMenu.js
@@ -6,8 +6,13 @@ const LunchMenu = () => {
   return (
     <div>
       <h3>Lunch Menu</h3>
-      <button>View</button>
-      <motion.p className="text-box"
+      <motion.button whileHover={{
+        scale: 3,
+        textShadow: '0px 0px 8px rgb(255,255,255)',
+        boxShadow:'0px 0px 8px rgb(255,255,255)' }}>
+        View
+      </motion.button>
+      {/* <motion.p className="text-box"
       initial={{ y: '-250px' }}
       animate={{ y: '100px' }}
       transition={{
@@ -16,7 +21,7 @@ const LunchMenu = () => {
         type: 'spring',
         stiffness: 500 // only works with spring
       }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a volutpat nibh. Vivamus condimentum ante eros, eget cursus metus ultrices vel. Aenean dapibus volutpat molestie. Maecenas nisl nibh, accumsan at felis nec, ultricies placerat sapien. Morbi pellentesque tincidunt bibendum. Proin eget vehicula purus, non sodales purus. Vestibulum pharetra vehicula dolor, a consequat turpis suscipit vel. Maecenas blandit sapien purus, a semper erat mattis ut. In lacinia non leo vel maximus. Quisque luctus gravida neque, volutpat sodales leo venenatis vehicula.</motion.p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a volutpat nibh. Vivamus condimentum ante eros, eget cursus metus ultrices vel. Aenean dapibus volutpat molestie. Maecenas nisl nibh, accumsan at felis nec, ultricies placerat sapien. Morbi pellentesque tincidunt bibendum. Proin eget vehicula purus, non sodales purus. Vestibulum pharetra vehicula dolor, a consequat turpis suscipit vel. Maecenas blandit sapien purus, a semper erat mattis ut. In lacinia non leo vel maximus. Quisque luctus gravida neque, volutpat sodales leo venenatis vehicula.</motion.p> */}
     </div>
   )
 }
--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView  } from 'react-intersection-observer';
+import './contact.scss';
 
 const Contact = () => {
+  const controls = useAnimation();
+  const [ sectionRef, sectionInView ] = useInView({
+    triggerOnce: false,
+    threshold: 0.7,
+  });
+
+  const sectionVariants = {
+    visible: { opacity: 1, backgroundColor: 'pink', transition: { duration: 1 } },
+    hidden: { opacity: 0,}
+  };
+  
+
+  useEffect(() => {
+    if (sectionInView) {
+      console.log('in view');
+      controls.start("visible");
+    }
+  }, [controls, sectionInView]);
+
   return (
-    <section id="contact">
+    <motion.section ref={sectionRef} id="contact" variants={sectionVariants} animate={controls} initial="hidden">
       <h1>Contact page</h1>
-    </section>
+      <p>Phone number</p>
+      <p>Email</p>
+    </motion.section>
   )
 }
 
