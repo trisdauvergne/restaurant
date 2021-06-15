85816b7085cafc83389c21f38c3489623add7459
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-15 15:11:26 +0200

    Updated about text styling

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <section className="about" id="about">
       {/* <h1 className="restaurant-name">Restaurant</h1> */}
       <article className="about-copy">
-        <p>About Restaurant</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque porttitor pharetra urna, eget consectetur nulla commodo ac. Donec imperdiet rhoncus enim, sed tempor nunc tristique vitae. Praesent ut nunc in ipsum pretium scelerisque in at justo. Curabitur vitae mi nec ligula suscipit lacinia ut ac lacus. Praesent luctus turpis nec odio tempus, non egestas dui tincidunt. Duis finibus nec metus vitae euismod. Vestibulum accumsan nec leo quis ornare. Phasellus sollicitudin iaculis elit, hendrerit mollis purus hendrerit ac. Ut ullamcorper, neque id elementum iaculis, massa nibh dictum magna, nec lobortis lorem turpis et ante.</p>
       </article>
     </section>
   )
