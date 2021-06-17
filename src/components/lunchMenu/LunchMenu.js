96187f5abf87a472f58cac313928f833648982bc
Tristan Dauvergne <tristan.dauvergne@gmail.com>
2021-06-17 14:15:59 +0200

    Testing how animatepresence works on deployed version

--- src/components/lunchMenu/LunchMenu.js
+++ src/components/lunchMenu/LunchMenu.js
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './lunchmenu.scss';
 import MenuItem from '../menuItem/MenuItem';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const LunchMenu = ({menu}) => {
   const [menuVisible, setMenuVisible] = useState(false);
+  const [test, setTest] = useState(true);
+
+  setTimeout(() => {
+    setTest(false);
+  }, 3000);
 
   // useEffect(() => {
   //   setLunchItems(menu);
   // }, []);
 
-  const clicked = () => {
-    console.log('clicked');
-    setMenuVisible(true);
-  }
-
   return (
     <section className="lunch-section">
-      <h3>Lunch Menu</h3>
-      <button onClick={clicked}>View</button>
-      {menuVisible && menu && menu.map(item => <><MenuItem menuitem={item} />
+      <AnimatePresence>
+      {test && <motion.h3 exit={{y: -1000}}>Lunch Menu</motion.h3>}
+      </AnimatePresence>
+      <motion.button onClick={() => setMenuVisible(!menuVisible)} whileHover={{scale: 4, transition: {duration: 0.3, yoyo: Infinity}}}>View</motion.button>
+      {menuVisible && menu && menu.map(item => 
+      <>
+        <MenuItem menuitem={item} />
       </>)}
     </section>
   )
--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import './menuitem.scss';
 
 const MenuItem = ({ menuitem }) => {
   const [visibleIngredients, setVisibleIngredients] = useState(false);
   return (
-    <div className="menu-item">
-      <p>{menuitem.dishName}</p>
-      <p>{menuitem.dishDescription}</p>
-      <p>{menuitem.dishPrice} SEK</p>
-      <button onClick={() => setVisibleIngredients(!visibleIngredients)}>See ingredients</button>
-      {visibleIngredients && menuitem.dishIngredients.map(ingredient => <><p>{ingredient}</p></>)}
-    </div>
+      <div className="menu-item" initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 1}}} exit={{y: -1000}}>
+        <p>{menuitem.dishName}</p>
+        <p>{menuitem.dishDescription}</p>
+        <p>{menuitem.dishPrice} SEK</p>
+        <button onClick={() => setVisibleIngredients(!visibleIngredients)}>See ingredients</button>
+        <AnimatePresence>
+        {visibleIngredients && menuitem.dishIngredients.map(ingredient => <><motion.p exit={{opacity: 0, transition: {duration: 1}}}>{ingredient}</motion.p></>)}
+        </AnimatePresence>
+      </div>
   )
 }
 
